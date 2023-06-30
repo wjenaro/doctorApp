3acd618b3492ff02296ff6ff44ce0f3466081a24
Jenaro Wachira <wjenaro@gmail.com>
2023-06-30 22:52:08 +0300

    good learning usercreations

--- baseApp/models.py
+++ baseApp/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class User(models.Model):
     user_id=models.AutoField(primary_key=True)
     username=models.TextField(max_length=100)
+    email=models.TextField(max_length=100)
     password=models.TextField(max_length=100)
     #will include other fields later 
--- baseApp/views.py
+++ baseApp/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
 
 
 def home(request):
     return render(request, "baseApp/index.html")
+def createUser(request):
+    form=SignUpForm()
+    if request.method == "POST":
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            raw_password=form.cleaned_data.get('password1')
+            user= authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+        else:
+            print ("return here")
+
+
+
+    return render(request, "baseApp/login-register.html", {"form": form})
