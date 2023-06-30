4cb20dbd380e12c62e975a2ec3138f8210538dd0
Jenaro Wachira <wjenaro@gmail.com>
2023-06-30 21:49:02 +0300

    form creations

--- DAppointment/settings.py
+++ DAppointment/settings.py
@@ -52,11 +52,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'DAppointment.urls'
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- baseApp/models.py
+++ baseApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id=models.AutoField(primary_key=True)
+    username=models.TextField(max_length=100)
+    password=models.TextField(max_length=100)
+    #will include other fields later 
